bc31ffd84eba435302f9c644f360ac70d6ccade7
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2023-01-07 13:24:00 +0800

    Init Comments

---
+++ index.js
@@ -0,0 +1,11 @@
+const gitalk = new Gitalk({
+    clientID: 'ba60ca5457edd408cc31',
+    clientSecret: 'f65f2a9640fde08792705ec2dd3fd3c08f66e67c',
+    repo: 'KaiHuaDou.github.com',
+    owner: 'KaiHuaDou',
+    admin: ['KaiHuaDou'],
+    id: location.pathname,
+    distractionFreeMode: false
+});
+
+gitalk.render('gitalk-container')
